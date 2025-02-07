238fc55d1f870e563e74306a081c076ceef237dd
liang <1791574028@qq.com>
2025-02-07 22:15:41 +0800

    添加chainsmith

--- rag.py
+++ rag.py
@@ -1,6 +1,14 @@
+import os
+
 from langchain_ollama import OllamaLLM
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.output_parsers import StrOutputParser
+
+os.environ["LANGSMITH_TRACING"] = "true"
+os.environ["LANGSMITH_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGSMITH_PROJECT"] = "liang-lang-smith"
+os.environ["LANGSMITH_API_KEY"]="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"
+
 
 model = OllamaLLM(model="llama3.1")
 parser = StrOutputParser()
